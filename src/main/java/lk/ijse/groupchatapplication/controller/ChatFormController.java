6f93f77f41887ca5eb6f7028f91c772d7105abdc
shamodha-sahan-rathnamalala <shamodhasrathnamalala@gmail.com>
2023-06-08 17:02:54 +0530

    updating

--- src/main/java/lk/ijse/groupchatapplication/controller/ChatFormController.java
+++ src/main/java/lk/ijse/groupchatapplication/controller/ChatFormController.java
@@ -1,12 +1,18 @@
 package lk.ijse.groupchatapplication.controller;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 
+import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -46,9 +52,9 @@ public class ChatFormController implements Initializable {
                         try {
                             String message = dataInputStream.readUTF();
                             if (message.equals("image")){
-
+                                receivingImage();
                             }else {
-
+                                receivingTextMessage(message);
                             }
                         } catch (IOException e) {
                             e.printStackTrace();
@@ -57,9 +63,42 @@ public class ChatFormController implements Initializable {
                 }
             }).start();
         } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void receivingTextMessage(String message) {
+
+    }
+
+    private void receivingImage() {
+        try {
+            String utf = dataInputStream.readUTF();
+            int size = dataInputStream.readInt();
+            byte[] bytes = new byte[size];
+            dataInputStream.readFully(bytes);
+            System.out.println(userName + "- Image received: from " + utf);
+
+            HBox hBox = new HBox();
+            Label messageLbl = new Label(utf);
+            messageLbl.setStyle("-fx-background-color:   #2980b9;-fx-background-radius:15;-fx-font-size: 18;-fx-font-weight: normal;-fx-text-fill: white;-fx-wrap-text: true;-fx-alignment: center;-fx-content-display: left;-fx-padding: 10;-fx-max-width: 350;");
+            hBox.setStyle("-fx-fill-height: true; -fx-min-height: 50; -fx-pref-width: 520; -fx-max-width: 520; -fx-padding: 10; " + (sender.equals(client.getName()) ? "-fx-alignment: center-right;" : "-fx-alignment: center-left;"));
+
+            Platform.runLater(()->{
+                ImageView imageView = new ImageView(new Image(new ByteArrayInputStream(bytes)));
+                imageView.setStyle("-fx-padding: 5px;");
+                imageView.setFitHeight(200);
+                imageView.setFitWidth(120);
+
+                hBox.getChildren().addAll(messageLbl, imageView);
+
+                // need add sc pane
+            });
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
+
     @FXML
     public void messageSendOnAction(ActionEvent actionEvent) {
 
