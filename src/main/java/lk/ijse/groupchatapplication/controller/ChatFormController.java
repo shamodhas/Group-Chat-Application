20b00aad805f7157cab147f319288413c9d6eadc
shamodha-sahan-rathnamalala <shamodhasrathnamalala@gmail.com>
2023-06-08 17:14:16 +0530

    updating

--- src/main/java/lk/ijse/groupchatapplication/controller/ChatFormController.java
+++ src/main/java/lk/ijse/groupchatapplication/controller/ChatFormController.java
@@ -11,13 +11,13 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
-import java.io.ByteArrayInputStream;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 import java.net.URL;
+import java.nio.file.Files;
 import java.util.ResourceBundle;
 
 /**
@@ -45,6 +45,7 @@ public class ChatFormController implements Initializable {
             this.dataInputStream = new DataInputStream(socket.getInputStream());
             this.dataOutputStream = new DataOutputStream(socket.getOutputStream());
             dataOutputStream.writeUTF(userName);
+            dataOutputStream.flush();
             new Thread(new Runnable() {
                 @Override
                 public void run() {
@@ -68,7 +69,12 @@ public class ChatFormController implements Initializable {
     }
 
     private void receivingTextMessage(String message) {
-
+        HBox hBox = new HBox();
+        hBox.setStyle("-fx-alignment: center-left;-fx-fill-height: true;-fx-min-height: 50;-fx-pref-width: 520;-fx-max-width: 520;-fx-padding: 10");
+        Label messageLbl = new Label(message);
+        messageLbl.setStyle("-fx-background-color:   #2980b9;-fx-background-radius:15;-fx-font-size: 18;-fx-font-weight: normal;-fx-text-fill: white;-fx-wrap-text: true;-fx-alignment: center-left;-fx-content-display: left;-fx-padding: 10;-fx-max-width: 350;");
+        hBox.getChildren().add(messageLbl);
+//        Platform.runLater(() -> need add sc pane);
     }
 
     private void receivingImage() {
@@ -82,7 +88,7 @@ public class ChatFormController implements Initializable {
             HBox hBox = new HBox();
             Label messageLbl = new Label(utf);
             messageLbl.setStyle("-fx-background-color:   #2980b9;-fx-background-radius:15;-fx-font-size: 18;-fx-font-weight: normal;-fx-text-fill: white;-fx-wrap-text: true;-fx-alignment: center;-fx-content-display: left;-fx-padding: 10;-fx-max-width: 350;");
-            hBox.setStyle("-fx-fill-height: true; -fx-min-height: 50; -fx-pref-width: 520; -fx-max-width: 520; -fx-padding: 10; " + (sender.equals(client.getName()) ? "-fx-alignment: center-right;" : "-fx-alignment: center-left;"));
+            hBox.setStyle("-fx-fill-height: true; -fx-min-height: 50; -fx-pref-width: 520; -fx-max-width: 520; -fx-padding: 10; " + (utf.equals(userName) ? "-fx-alignment: center-right;" : "-fx-alignment: center-left;"));
 
             Platform.runLater(()->{
                 ImageView imageView = new ImageView(new Image(new ByteArrayInputStream(bytes)));
@@ -101,12 +107,53 @@ public class ChatFormController implements Initializable {
 
     @FXML
     public void messageSendOnAction(ActionEvent actionEvent) {
-
+        try {
+            String textMessage = txtMessage.getText();
+            if (textMessage != null){
+                HBox hBox = new HBox();
+                hBox.setStyle("-fx-alignment: center-right;-fx-fill-height: true;-fx-min-height: 50;-fx-pref-width: 520;-fx-max-width: 520;-fx-padding: 10");
+                Label messageLbl = new Label(textMessage);
+                messageLbl.setStyle("-fx-background-color:  purple;-fx-background-radius:15;-fx-font-size: 18;-fx-font-weight: normal;-fx-text-fill: white;-fx-wrap-text: true;-fx-alignment: center-left;-fx-content-display: left;-fx-padding: 10;-fx-max-width: 350;");
+                hBox.getChildren().add(messageLbl);
+//                vBox.getChildren().add(hBox); need add sc pane
+                dataOutputStream.writeUTF(textMessage);
+                dataOutputStream.flush();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
     public void imgSendOnAction(ActionEvent actionEvent) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select Image ");
+        FileChooser.ExtensionFilter imageFilter = new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg");
+        fileChooser.getExtensionFilters().add(imageFilter);
+        File selectedFile = fileChooser.showOpenDialog(new Stage());
+        if (selectedFile != null) {
+            try {
+                byte[] bytes = Files.readAllBytes(selectedFile.toPath());
+                HBox hBox = new HBox();
+                hBox.setStyle("-fx-fill-height: true; -fx-min-height: 50; -fx-pref-width: 520; -fx-max-width: 520; -fx-padding: 10; -fx-alignment: center-right;");
 
+                // Display the image in an ImageView or any other UI component
+                ImageView imageView = new ImageView(new Image(new FileInputStream(selectedFile)));
+                imageView.setStyle("-fx-padding: 10px;");
+                imageView.setFitHeight(180);
+                imageView.setFitWidth(100);
+
+                hBox.getChildren().addAll(imageView);
+//                vBox.getChildren().add(hBox); need add sc pane
+
+                dataOutputStream.writeUTF("*image*");
+                dataOutputStream.writeInt(bytes.length);
+                dataOutputStream.write(bytes);
+                dataOutputStream.flush();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
 
