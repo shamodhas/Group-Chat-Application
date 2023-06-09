d1c7184fd1daaf73ddbf4352470e9fdc81b5fef5
shamodha-sahan-rathnamalala <shamodhasrathnamalala@gmail.com>
2023-06-09 10:48:44 +0530

    updating

--- src/main/java/lk/ijse/groupchatapplication/controller/LoginFormController.java
+++ src/main/java/lk/ijse/groupchatapplication/controller/LoginFormController.java
@@ -2,14 +2,11 @@ package lk.ijse.groupchatapplication.controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import lk.ijse.groupchatapplication.ClientHandler;
 
 import java.io.IOException;
-import java.net.URL;
 
 public class LoginFormController {
     @FXML
@@ -18,14 +15,8 @@ public class LoginFormController {
     @FXML
     protected void onHelloButtonClick() throws IOException {
         Stage stage = (Stage) txtUserName.getScene().getWindow();
-        FXMLLoader fxmlLoader = new FXMLLoader();
         ChatFormController.userName = txtUserName.getText();
-
         stage.setScene(new Scene(FXMLLoader.load(this.getClass().getResource("/lk/ijse/groupchatapplication/view/ChatForm.fxml"))));
-        stage.setOnCloseRequest(event -> {
-//            ChatFormController.close();
-            System.exit(0);
-        });
         stage.show();
     }
 }
